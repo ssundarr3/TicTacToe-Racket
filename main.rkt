b0cf2d7805687bb54fee22de5bc4bc51cd8f447c
Shridhar Sundarraj <shridhar_3@hotmail.com>
2016-01-13 19:20:56 -0500

    First Commit

---
+++ main.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+#|
+1) Download all files
+2) Run ttt.rkt
+3) In the interactions window, enter one of the following: 
+(main player-first) ;;for player to play first
+(main comp-first)   ;; for computer to play first
+|#
+
+(define player-first (list 'p '() '() '(1 2 3 4 5 6 7 8 9)))
+(define comp-first (list 'c '() '() '(1 2 3 4 5 6 7 8 9)))
+
+(require "./assets/ttt.rkt")
+
+#|To Add...
+Continuously play games
+keep track of games lost, tied and won ...
+Themes?
+Clock? for time played, time to think per move ???
+wait message (computer thinking..., play now/ your turn...)
+Web?
+On same spot and on computer's prev spots, disallow.. Also  when comp thinking?)
+learn, impl ab pruning? (equivalent tree or worse => don't go??, <-1 => no good ??)
+|#
+
