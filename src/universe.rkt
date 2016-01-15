b14e0f27f00845b8965b6f5e29592bc4e639b54a
Shridhar Sundarraj <shridhar_3@hotmail.com>
2016-01-15 15:27:02 -0500

    Added "Your turn", "Computer thinking" messages...

--- src/universe.rkt
+++ src/universe.rkt
@@ -11,53 +11,55 @@
 (define box (rectangle size size 'outline 'black))
 (define 3-boxes (beside box box box))
 (define display-text (text "Hello" 24 "orange"))
-(define ttt-brd (above 3-boxes 3-boxes 3-boxes display-text))
+(define text-area (rectangle (* 3 size) (/ size 3)  'outline 'white))
+(define ttt-brd (above 3-boxes 3-boxes 3-boxes text-area))
 
 
 (define (display-moves brd x y me)
-  (local (;Number String -> Boolean
-          ;Is the mouse clicking close to a number
+  (local (;; Nat Str -> Bool
           (define (on-sq? n)
             (and (<= (abs (- (first (gridsq n)) x))(/ size 2))
                  (<= (abs (- (second (gridsq n)) y)) (/ size 2))
                  (string=? me "button-down")))
-          ;Number -> Board
-          ;makes a new board
+          ;; Nat -> Board
+          ;; makes a new board
           (define (create-board n)
             (list 'c
                   (second brd)
                   (append (third brd) (list n))
                   (remove n (fourth brd)))))
-        (cond
-          [(on-sq? 1)(create-board 1)]
-          [(on-sq? 2)(create-board 2)]
-          [(on-sq? 3)(create-board 3)]
-          [(on-sq? 4)(create-board 4)]
-          [(on-sq? 5)(create-board 5)]
-          [(on-sq? 6)(create-board 6)]
-          [(on-sq? 7)(create-board 7)]
-          [(on-sq? 8)(create-board 8)]
-          [(on-sq? 9)(create-board 9)]
-          [else brd])))
-
+    (cond
+      [(on-sq? 1)(create-board 1)]
+      [(on-sq? 2)(create-board 2)]
+      [(on-sq? 3)(create-board 3)]
+      [(on-sq? 4)(create-board 4)]
+      [(on-sq? 5)(create-board 5)]
+      [(on-sq? 6)(create-board 6)]
+      [(on-sq? 7)(create-board 7)]
+      [(on-sq? 8)(create-board 8)]
+      [(on-sq? 9)(create-board 9)]
+      [else brd])))
 
 (define (initialize brd)
-  (foldr p-moves
-         (foldr c-moves
-                ttt-brd
-                (second brd)) 
-         (third brd)))
+  (place-image
+   (text (cond[(symbol=? 'p (first brd))  "Your Turn"]
+              [else "Computer thinking"])
+         (floor (/ size 6)) 'green)
+   (floor (* 1.5 size))
+   (floor (+ (* 3.15 size) ))
+   (foldr p-moves
+          (foldr c-moves ttt-brd (second brd))
+          (third brd))))
 
-;Number Image -> Image
-;places an 'X' onto the tic tac toe board
+
+;; Nat Img -> Img
 (define (c-moves n im)
   (place-image (text "O" (/ size 2) 'blue)
                (first (gridsq n))
                (second (gridsq n))
                im))
 
-;Number Image -> Image
-;places an 'O' onto the tic tac toe board
+;; Num Img -> Img
 (define (p-moves n im)
   (place-image (text "X" (/ size 2) 'red)
                (first (gridsq n))
@@ -65,8 +67,7 @@
                im))
 
 
-;Number -> Posn
-;puts a number at a posn
+;; Nat -> (listof Num Num)
 (define (gridsq n)
   (cond
     [(= n 1)(grid 1 1)]
